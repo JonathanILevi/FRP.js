a8cdbcb6ba99025f811bc4dea8efadf1f4d0abe6
Jonathan <JonathanILevi@gmail.com>
2025-01-17 09:38:33 -0600

    New, more idealistic cell switch code.

--- modules/FRP/Constant/CellSwitch.m.js
+++ modules/FRP/Constant/CellSwitch.m.js
@@ -1,6 +1,5 @@
 import {Cell,makeCell} from "../Base/Cell.m.js";
 import {EnumCell} from "./ConstantCell.m.js";
-import {partialRoot,joinRoots} from "../Core/PushRoot.m.js";
 
 EnumCell.prototype.switch = function() {
 	if (this.possibilities.all(v=>v instanceof Cell))
@@ -11,35 +10,23 @@ EnumCell.prototype.switch = function() {
 }
 // I don't think this was coded right at all.  This new code has not been tested.
 // Also note, that this does no pausing of unused cellThreads, as that and that never happens in other functions, so that is normal.  It would make sense to make pausable cells, but then would only work their value would be recalculated whenever it would need resumed. (Basically the switch would have to include a partial root of each pausable inner cell)
+// And...I rewrote it again, much more idealistic, using other functions.
+// Also note, that FRP.js has poor handling for caching currently.  the `current` variable here, could just make this caching, and then use the cached value.  I think this is true throughout.
 EnumCell.prototype.switchC = function() {
-	let nnid = Symbol();
 	let current = this.initial;
-	let n = makeCell(this.initial.initial,joinRoots(nnid,[this._root,...this.possibilities.map(p=>p._root)]),nnid);
-	
-	this.map(v=>{
-		this.current = v;
-		n._root.send(v.grab());
-	});
-	this.possibilities.forEach(p=>p.forEach(pv=>{
-		n._root.send(pv);
-	}));
-	
-	return n;
+	return merge(
+		this.map(v=>{
+			current = v;
+			return v.grab();
+		}),
+		...this.possibilities.map(p=>p.changes().filter(()=>p==current)),
+	);
 }
 // Modified form new switchC, also not tested.
 EnumCell.prototype.switchS = function() {
-	let nnid = Symbol();
 	let current = this.initial;
-	let n = makeStream(joinRoots(nnid,this.possibilities.map(p=>p._root)),nnid);
-	
-	this.map(v=>{
-		this.current = v;
-	});
-	this.possibilities.forEach(p=>p.forEach(pv=>{
-		n._root.send(pv);
-	}));
-	
-	return n;
+	this.forEach(v=>current = v);
+	return merge(...this.possibilities.map(p=>p.changes().filter(()=>p==current)));
 }
 
 
