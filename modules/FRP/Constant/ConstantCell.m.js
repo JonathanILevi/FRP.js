cfabbbef8c9f33311570a805f573f3f96d216f03
Jonathan <JonathanILevi@gmail.com>
2025-01-17 09:58:45 -0600

    Lots of old code refactored and new code for constant cells.

--- modules/FRP/Constant/ConstantCell.m.js
+++ modules/FRP/Constant/ConstantCell.m.js
@@ -2,8 +2,8 @@ import {Cell,} from "../Base/Cell.m.js";
 import {newRoot,newDeadRoot,joinRoots,joinRootsMap,partialRoot,compareRoots,same,overlapping,discrete} from "../Core/PushRoot.m.js";
 
 export {ConstantCell,constantCell,makeConstantCell,};
-export {RootConstantChangingCell,ConstantChangingCell,constantChangingCell,makeConstantChangingCell,};
-export {RootEnumCell,EnumCell,enumCell,makeEnumCell,};
+export {ConstantChangingCell,constantChangingCell,makeConstantChangingCell,};
+export {EnumCell,enumCell,makeEnumCell,};
 
 
 class EnumCell extends Cell {
@@ -11,24 +11,12 @@ class EnumCell extends Cell {
 		super(initial,...args);
 		this.possibilities = possibilities;
 	}
-	map(f) {
+	pureMap(f) {
 		let n = makeEnumCell(f(this.initial),this.possibilities.map(f),this._root);
 		this._root.addNode((scope)=>{
 			scope[n.nodeIdentifier]=f(scope[this.nodeIdentifier]);
 		});
 		return n;
-	}
-}
-class RootEnumCell extends EnumCell {
-	constructor(initial, possibilities) {
-		let nodeId = Symbol();
-		super(initial, possibilities, newRoot(nodeId), nodeId);
-	}
-	change(value) {
-		if (this.possibilities.indexOf(value)!=-1)
-			this._root.send(value);
-		else
-			console.assert(false);
 	}
 }
 
@@ -45,15 +33,6 @@ class ConstantChangingCell extends EnumCell {
 			scope[n.nodeIdentifier]=n.value;
 		});
 		return n;
-	}
-}
-class RootConstantChangingCell extends ConstantChangingCell {
-	constructor(value) {
-		let nodeId = Symbol();
-		super(value,newRoot(nodeId),nodeId);
-	}
-	change() {
-		this._root.send(this.value);
 	}
 }
 
@@ -73,23 +52,35 @@ function constantCell(value) {
 ConstantCell.cell = constantCell;
 Cell.constant = constantCell;
 
-function constantChangingCell(value) {
-	return new RootConstantChangingCell(value);
+function constantChangingCell(value, changeOut=null) {
+	let cell = makeConstantChangingCell(value);
+	cell.change = v=>{
+		console.assert(v==value);
+		cell._root.send(v);
+	};
+	if (changeOut)
+		changeOut(cell.change);
+	return cell;
 }
 ConstantChangingCell.cell = constantChangingCell;
 Cell.changingConstant = constantChangingCell;
 
-function enumCell(initial,possibilities) {
-	if (possibilities.length==0)
-		console.assert(false, "Cannot have an EnumCell without any possible values.");
-	else if (possibilities.indexOf(initial)==-1)
-		console.assert(false, "Inital value must be in possible values.");
-	else {
-		if (possibilities.length==1)
-			return constantCell(possibilities[0]);
-		else
-			return new RootEnumCell(initial,possibilities);
-	}
+function enumCell(initial, possibilities, changeOut=null) {
+	console.assert(possibilities.length!=0, "Cannot have an EnumCell without any possible values.");
+	console.assert(possibilities.includes(initial),"Inital value must be in possible values.");
+	
+	if (possibilities.length==1)
+		return constantCell(possibilities[0]);
+	
+	let cell = makeEnumCell(initial,possibilities);
+	cell.change = v=>{
+		console.assert(possibilities.includes(v), "Value must be in possible values.");
+		cell._root.send(v);
+	};
+	if (changeOut)
+		changeOut(cell.change);
+	
+	return cell;
 }
 EnumCell.cell = enumCell;
 Cell.enum = enumCell;
@@ -112,7 +103,7 @@ function makeEnumCell(initial,possibilities,root=null,nnid=null) {
 	if (possibilities.length==0)
 		console.assert(false, "Cannot have an EnumCell without any possible values.");
 	else if (possibilities.indexOf(initial)==-1)
-		console.assert(false, "Inital value must be in possible values.");
+		console.assert(false, "Inital value must be in possible values.", initial, possibilities);
 	else {
 		if (possibilities.length==1)
 			return makeConstantChangingCell(possibilities[0],root,nnid);
